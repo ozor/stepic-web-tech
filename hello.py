deb7b57d45283ce278e9448d7d38e8969b681535
Denis <ozor@yandex.ru>
2016-11-18 00:27:25 +0700

    add files for new task

--- hello.py
+++ hello.py
@@ -1,9 +1,17 @@
+from cgi import parse_qs
+
 def wsgi_application(environ, start_response):
-    status = '200 OK'
-    headers = [
-        ('Content-Type', 'text/plain')
-    ]
-    start_response(status, headers)
-    # print(environ)
-    # return environ[0]
-    return ["\n".join(environ["QUERY_STRING"].split("&"))]
+
+  query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
+  body = []
+  for key, values in query.items():
+    for item in values:
+      body.append(key + "=" + item + "\r\n")
+   
+  status = '200 OK'
+  headers = [
+   ('Content-Type', 'text/plain')
+  ]
+  
+  start_response(status, headers)
+  return body
